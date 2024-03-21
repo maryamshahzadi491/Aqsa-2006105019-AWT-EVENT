e6d55d393fc3d1bd97e4a14a2fce4fe5070adef5
maryamshahzadi491 <163430256+maryamshahzadi491@users.noreply.github.com>
2024-03-21 12:01:28 -0600

    Add files via upload

---
+++ Aqsa_event.js
@@ -0,0 +1,17 @@
+function update(){
+    console.log("event is fired");
+}
+// I want to create variable
+
+var x =10;
+var y =20;
+
+console.log(x*y);
+
+//I want to create in js
+function sum(a,b){
+    return a+b;
+}
+// if i want to create to assign function to variable 
+var mysum= sum;
+console.log(mysum);
